a60920a22c789341b6457b687a568a6bc1151494
Sergey Kurdyukov <sergey.kurdyukov.v@gmail.com>
2021-04-20 21:09:03 +0300

    The version option has been done

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('Hello World!');
+const {Cli} = require(`./cli`);
+
+const optionName = process.argv[2];
+
+console.log(`Option is:`, optionName);
+switch (optionName) {
+  case `--version`:
+    const version = Cli[optionName].run();
+    console.info(`The current version is: ${version}`);
+    break;
+  default:
+    console.error(`Unknown option`);
+}
