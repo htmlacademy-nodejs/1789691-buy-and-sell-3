feffeb5c8f181b894e9fc972b6a3173310b4c571
Sergey Kurdyukov <sergey.kurdyukov.v@gmail.com>
2021-05-05 16:23:57 +0300

    The server has been created. It reads content from the mock.json and returns title list.

---
+++ src/service/cli/server.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const chalk = require(`chalk`);
+const http = require(`http`);
+const fs = require(`fs`).promises;
+
+const { DEFAULT_PORT, FILE_NAME, HttpCodes } = require(`../constants`);
+
+const sendRequest = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+  res.writeHead(statusCode, {
+    'Content-Type': 'text/html; charset=UTF8'
+  });
+  res.end(template);
+}
+
+const onClientConnect = async (req, res) => {
+  const notFoundMessage = `Not found`;
+
+  switch(req.url) {
+    case `/`:
+      try {
+        const content = await fs.readFile(FILE_NAME);
+        const mocks = JSON.parse(content);
+        const listItems = mocks.map((item) => `<li>${item.title}</li>`).join(``);
+        sendRequest(res, HttpCodes.OK, `<ul>${listItems}</ul>`);
+      } catch (error) {
+        sendRequest(res, HttpCodes.NOT_FOUND, notFoundMessage);
+      }
+      break;
+    default:
+      sendRequest(res, HttpCodes.NOT_FOUND, notFoundMessage);
+  }
+}
+
+module.exports = {
+  name: `--server`,
+  run(args) {
+    const [customPort] = args;
+    const port = Number(customPort) || DEFAULT_PORT;
+    const server = http.createServer(onClientConnect)
+      .listen(port)
+      .on(`listening`, (error) => {
+        if (error) {
+          return console.error(chalk.red(`Creation server error:`), error);
+        }
+
+        return console.info(chalk.green(`Wait for connection on ${port} port`));
+      });
+  },
+};
--- src/service/constants.js
+++ src/service/constants.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const DEFAULT_COMMAND = `--help`;
 const DEFAULT_COUNT = 1;
+const DEFAULT_PORT = 3000;
 const FILE_NAME = `mocks.json`;
+const MAX_OFFER_COUNT = 1000;
+const USER_ARGV_INDEX = 2;
 
 const OfferType = {
   OFFER: `offer`,
@@ -18,23 +22,29 @@ const SumRestrict = {
   MAX: 100000,
 };
 
-const USER_ARGV_INDEX = 2;
-const DEFAULT_COMMAND = `--help`;
-const MAX_OFFER_COUNT = 1000;
-
 const ExitCode = {
   SUCCESS: 0,
   FAIL: 1,
 };
 
+const HttpCodes = {
+  OK: 200,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  UNAUTHORIZED: 401,
+}
+
 module.exports = {
   DEFAULT_COMMAND,
   DEFAULT_COUNT,
+  DEFAULT_PORT,
   FILE_NAME,
   MAX_OFFER_COUNT,
   USER_ARGV_INDEX,
   ExitCode,
   OfferType,
+  HttpCodes,
   PictureRestrict,
   SumRestrict,
 };
